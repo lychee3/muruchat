6cd8f57bf777495d1aab5d7db474e2765a0ae25d
koyamaru <koyamaru@gmail.com>
2019-05-18 00:27:00 +0900

    commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # muruchat
+
+## OpenShiftでデプロイする方法
+oc new-app https://github.com/lychee3/muruchat
+
+oc expose -f openshift/muruchat-svc.yaml
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-const PORT = process.env.PORT || 7000;
+const PORT = 7000;
 
 app.get('/' , function(req, res){
    res.send('hello world');
