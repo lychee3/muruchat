c2e6577234812ab5d5c832d944b191df1d0d9763
koyamaru <koyamaru@gmail.com>
2019-05-19 08:41:46 +0900

    commit

--- README.md
+++ README.md
@@ -2,11 +2,8 @@
 
 ## OpenShiftでデプロイする方法
 
-oc new-app https://github.com/lychee3/muruchat
+$ oc new-app https://github.com/lychee3/muruchat
 
-勝手に作られたserviceを削除する（手順がイケてないので要改善）
-oc delete service/muruchat
+$ oc expose svc/muruchat
 
-oc create -f openshift/muruchat-svc.yaml
-
-http://192.168.64.4:30001
+http://muruchat-myproject.192.168.64.4.nip.io
--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-const PORT = 8080;
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 8080;
 
 app.get('/' , function(req, res){
-   res.send('hello world');
+    res.sendFile(__dirname+'/index.html');
+});
+
+io.on('connection',function(socket){
+    console.log('connected');
 });
 
 http.listen(PORT, function(){
-  console.log('server listening. Port:' + PORT);
+    console.log('server listening. Port:' + PORT);
 });
