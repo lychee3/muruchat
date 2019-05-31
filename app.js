1232c5951695bcd25bfe9602cb94b6e0c76554cc
koyamaru <koyamaru@gmail.com>
2019-05-31 23:36:10 +0900

    commit

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const mysql = require('promise-mysql');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-//const PORT = process.env.PORT || 8080;
 const PORT = process.env.PORT || 8080;
 
 app.get('/' , function(req, res){
