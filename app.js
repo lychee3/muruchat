927c835c8e1e8b86c720fb489fff3e161176e84e
koyamaru <koyamaru@gmail.com>
2019-06-08 00:27:08 +0900

    commit

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http, {path: '/muruchat/socket.io'});
 const PORT = process.env.PORT || 8080;
 
-app.use('/muruchat', express.static('css'));
-app.use('/muruchat', express.static('js'));
+app.use('/muruchat', express.static('public'));
 
 app.get('/', (req, res) => {
     res.redirect('/muruchat');
